4a083e4cc44dff04b950e7c20209931aec376c03
gerard <gerardsori@gmail.com>
2024-02-21 19:36:59 +0100

    bb

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     [SerializeField] Transform gunPosition;
 
     [SerializeField] int bulletType = 0;
+    [SerializeField] int bulletType1 = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,8 +28,23 @@ public class Player : MonoBehaviour
             }
             else
             {
-                Debug.LogError("Pool demasiado pequeño");
+                Debug.LogError("Pool de bullet demasiado pequeño");
             }
         }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            GameObject bullet1 = PoolManager.Instance.GetPooledObjects(bulletType1, gunPosition.position, gunPosition.rotation);
+            
+            if(bullet1 != null)
+            {
+                bullet1.SetActive(true);
+            }
+            else
+            {
+                Debug.LogError("Pool de bullet1 demasiado pequeño");
+            }
+        }
+        
     }
 }
